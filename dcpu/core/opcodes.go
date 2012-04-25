7aefa2ea0763443014a5bf36c5cc955cd579e29c
Kevin Ballard <kevin@sb.org>
2012-04-24 20:07:31 -0700

    Define the values for the new instruction set

--- dcpu/core/opcodes.go
+++ dcpu/core/opcodes.go
@@ -2,30 +2,52 @@ package core
 
 // basic opcodes
 const (
-	opcodeSET = 0x1
-	opcodeADD = 0x2
-	opcodeSUB = 0x3
-	opcodeMUL = 0x4
-	opcodeDIV = 0x5
-	opcodeMOD = 0x6
-	opcodeSHL = 0x7
-	opcodeSHR = 0x8
-	opcodeAND = 0x9
-	opcodeBOR = 0xa
-	opcodeXOR = 0xb
-	opcodeIFE = 0xc
-	opcodeIFN = 0xd
-	opcodeIFG = 0xe
-	opcodeIFB = 0xf
+	opcodeSET = 0x01
+	opcodeADD = 0x02
+	opcodeSUB = 0x03
+	opcodeMUL = 0x04
+	opcodeMLI = 0x05
+	opcodeDIV = 0x06
+	opcodeDVI = 0x07
+	opcodeMOD = 0x08
+	opcodeAND = 0x09
+	opcodeBOR = 0x0a
+	opcodeXOR = 0x0b
+	opcodeSHR = 0x0c
+	opcodeASR = 0x0d
+	opcodeSHL = 0x0e
+	/* 0x0f is reserved */
+	opcodeIFB = 0x10
+	opcodeIFC = 0x11
+	opcodeIFE = 0x12
+	opcodeIFN = 0x13
+	opcodeIFG = 0x14
+	opcodeIFA = 0x15
+	opcodeIFL = 0x16
+	opcodeIFU = 0x17
 )
 
 // non-basic opcodes
 const (
 	opcodeJSR = 0x1
+	/* 0x02-0x07 are reserved */
+	opcodeINT = 0x08
+	opcodeIAG = 0x09 // spec says ING, but it's probably a typo
+	opcodeIAS = 0x0a // spec says INS, but it's probably a typo
+	/* 0x0b-0x0f are reserved */
+	opcodeHWN = 0x10
+	opcodeHWQ = 0x11
+	opcodeHWI = 0x12
 )
 
 // extended non-basic opcodes (internal representation)
 const (
-	opcodeExtJSR = 0x101
+	opcodeExtJSR = opcodeJSR | opcodeExtendedOffset
+	opcodeExtINT = opcodeINT | opcodeExtendedOffset
+	opcodeExtIAG = opcodeIAG | opcodeExtendedOffset
+	opcodeExtIAS = opcodeIAS | opcodeExtendedOffset
+	opcodeExtHWN = opcodeHWN | opcodeExtendedOffset
+	opcodeExtHWQ = opcodeHWQ | opcodeExtendedOffset
+	opcodeExtHWI = opcodeHWI | opcodeExtendedOffset
 )
 const opcodeExtendedOffset = 0x100
